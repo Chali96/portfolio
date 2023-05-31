fbbcf9fe4412e00602693e5c06b9ecea37de6d71
ChaliMsani <chalisani@hotmail.com>
2023-05-31 19:46:43 +0200

    background image for Home.js added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 const Home = () => {
     return (
-     <>
-     <div>
-        <h1>Home</h1>
-     </div>
-     </>
+      <div className="container">
+        <div className="background-image">
+          {/* Content for the first image */}
+        </div>
+        
+        <div className="overlay-image">
+          {/* Content for the second image */}
+          {/* Links, text, and icon links */}
+        </div>
+      </div>
     );
+  };
 
-};
-
-export default Home;
+  export default Home;
+  
