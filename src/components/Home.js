3d42613c0b808eefc1fd9fe8e5dbb08fe07e07f2
ChaliMsani <chalisani@hotmail.com>
2023-05-31 20:19:34 +0200

    added links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import leaveimg from './images/leaveimg.jpeg'
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
       <div className="container">
@@ -6,10 +8,21 @@ const Home = () => {
         
         <div className="overlay-image">
             <img className='leaveimg' src={leaveimg} />
+            <h2 className='info'>
+            Hello! I’m Chali Mohamedsani, a web developer student based in Stockholm.
+            </h2>
 
+            <div className="overlay-content">
+          <Link to="/" className="link">Home</Link>
+          <Link to="/page2" className="link">About Me</Link>
+          <Link to="/page3" className="link">Projects</Link>
+          <Link to="/page3" className="link">Contact Me</Link>
+
+          {/* Additional links, text, and icon links */}
         </div>
         </div>
 
+        </div>
       
       </div>
     );
