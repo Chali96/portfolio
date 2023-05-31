3656b86a7327e1cc8ef99145f0bf1d9951b7c8fc
ChaliMsani <chalisani@hotmail.com>
2023-05-31 20:01:38 +0200

    added overlay-image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
+
 const Home = () => {
     return (
       <div className="container">
-        <div className="background-image">
-          {/* Content for the first image */}
-        </div>
+        <div className="background-image"> 
         
         <div className="overlay-image">
-          {/* Content for the second image */}
           {/* Links, text, and icon links */}
         </div>
+        </div>
+
+      
       </div>
     );
   };
